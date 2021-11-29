e472368bec9a21bb8a8f3d6315f6256e2cde3201
jihami <w2027@e-mirim.hs.kr>
2021-11-30 02:01:07 +0900

    소스코드정리, 벽돌깨기-bgm

--- avoidSnow.py
+++ avoidSnow.py
@@ -2,58 +2,43 @@ import random
 import pygame
 from tkinter import *
 from tkinter import messagebox
-#############################################
 root= Tk()
 root.withdraw()
-#기본초기화(반드시 해야하는 것들)
-pygame.init() #초기화 (반드시 필요)
+pygame.init()
 
-#화면 크기 설정
-screen_width = 480 #가로 크기
-screen_height = 640 #세로 크기
+screen_width = 480
+screen_height = 640
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-#화면 타이틀 설정
 pygame.display.set_caption("눈 피하기")
 game_font = pygame.font.Font(None, 40)
 
-#FPS - Frame per Second - 초당 프레임수
 clock = pygame.time.Clock()
-#############################################
-
-#1.사용자 게임 초기화(배경화면, 게임 이미지, 좌표, 속도, 폰트 등)
-
-#배경 만들기
 BLACK = (0, 0, 0)
-# background = pygame.image.load("D:/Project_LPG/img/background.png")
 
-#캐릭터 만들기
 character = pygame.image.load("D:/Project_LPG/img/people.png")
 character_size = character.get_rect().size
 character_width = character_size[0]
 character_height = character_size[1]
-character_x_pos = (screen_width/2) - (character_width/2) #화면중앙
-character_y_pos = screen_height - character_height # 화면 최하단
-
-#이동 위치
+character_x_pos = (screen_width/2) - (character_width/2)
+character_y_pos = screen_height - character_height
 too_x = 0
 character_speed = 35
 
-#눈 만들기
 snow = pygame.image.load("D:/Project_LPG/img/snow.png")
 snow2 = pygame.image.load("D:/Project_LPG/img/snow.png")
 snow3 = pygame.image.load("D:/Project_LPG/img/snow.png")
 snow_size = snow.get_rect().size
 snow_width = snow_size[0]
 snow_height = snow_size[1]
-snow_x_pos = random.randint(0,(screen_width-snow_width)) #0~스크린크기-눈의 가로중 랜덤
-snow_y_pos = random.randint(0,0)
-snow_x2_pos = random.randint(0,(screen_width-snow_width)) #0~스크린크기-눈의 가로중 랜덤
+snow_x1_pos = random.randint(0, (screen_width - snow_width))
+snow_y1_pos = random.randint(0, 0)
+snow_x2_pos = random.randint(0,(screen_width-snow_width))
 snow_y2_pos = random.randint(0,30)
-snow_x3_pos = random.randint(0,(screen_width-snow_width)) #0~스크린크기-눈의 가로중 랜덤
+snow_x3_pos = random.randint(0,(screen_width-snow_width))
 snow_y3_pos = random.randint(0,50)
 
-game_font = pygame.font.Font(None, 40) #폰트 객체 생성(폰트, 크기)
+game_font = pygame.font.Font(None, 40)
 score = 0
 level = 1
 snow_speed = 10
@@ -62,16 +47,13 @@ sound = pygame.mixer.Sound("D:/Project_LPG/bgm/bgm.mp3")
 sound.play(-1)
 score_sound = pygame.mixer.Sound("D:/Project_LPG/bgm/score.wav")
 
-running = True #게임이 진행중인가?
-def Click():
-    messagebox.showinfo("알림칭", "알림창")
+running = True
+
 while running:
-    dt = clock.tick(30) #보통은 30사용
-    #2.이벤트 처리 (키보드, 마우스 등)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN: #키를 눌렀을 떄
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 too_x -= character_speed
             elif event.key == pygame.K_RIGHT:
@@ -80,19 +62,18 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 too_x = 0
 
-    #3.게임 캐릭터 위치 정의
     character_x_pos += too_x
 
-    if character_x_pos < 0: #화면 오른쪽 밖으로 나갈때
+    if character_x_pos < 0:
         character_x_pos = 0
-    elif character_x_pos > screen_width - character_width: #화면 왼쪽으로 나갈때
+    elif character_x_pos > screen_width - character_width:
         character_x_pos = screen_width - character_width
 
 
-    snow_y_pos += snow_speed #떨어지는 효과
-    if snow_y_pos > screen_height: #화면에서 밑으로 떨어졌을때
-        snow_y_pos = 0
-        snow_x_pos = random.randint(0, (screen_width-snow_width))
+    snow_y1_pos += snow_speed
+    if snow_y1_pos > screen_height:
+        snow_y1_pos = 0
+        snow_x1_pos = random.randint(0, (screen_width - snow_width))
     snow_y2_pos += snow_speed
     if snow_y2_pos > screen_height:
         snow_y2_pos = 0
@@ -107,8 +88,8 @@ while running:
     character_rect.top = character_y_pos
 
     snow_rect = snow.get_rect()
-    snow_rect.left = snow_x_pos
-    snow_rect.top = snow_y_pos
+    snow_rect.left = snow_x1_pos
+    snow_rect.top = snow_y1_pos
 
     snow2_rect = snow2.get_rect()
     snow2_rect.left = snow_x2_pos
@@ -118,7 +99,7 @@ while running:
     snow3_rect.left = snow_x3_pos
     snow3_rect.top = snow_y3_pos
 
-    if snow_y_pos == 0 :
+    if snow_y1_pos == 0 :
         score += 1
         score_sound.play()
     if snow_y2_pos == 0 :
@@ -144,22 +125,18 @@ while running:
         snow_speed = 13
         level = 2
 
-    sL = game_font.render(f"Score : {score}, Level : {level}", True, (255, 255, 255))  # 카운트당 스코어가 오르게하고 화면에 스코어 메세지를 출력하게 함
+    sL = game_font.render(f"SCORE : {score}, LEVEL: {level}", True, (255, 255, 255))
     sL_rect = sL.get_rect()
 
-    #5.화면 그리기기
-    # screen.blit(background, (0,0))
     screen.fill(BLACK)
     screen.blit(character,(character_x_pos,character_y_pos))
     screen.blit(snow3, (snow_x3_pos, snow_y3_pos))
     screen.blit(snow2, (snow_x2_pos, snow_y2_pos))
-    screen.blit(snow, (snow_x_pos, snow_y_pos))
-    screen.blit(sL, sL_rect)  # 스코어 메세지 출력
-
-    pygame.display.update() #게임화면 다시그리기 -> 안그리면 배경 적용 x
-
-def Click():
+    screen.blit(snow, (snow_x1_pos, snow_y1_pos))
+    screen.blit(sL, sL_rect)
+    pygame.display.update()
+    clock.tick(30)
+def message():
     messagebox.showinfo("종료", f"점수 : {score} , 단계 : {level} \n 종료되었습니다.")
-Click()
-# pygame 종료
+message()
 pygame.quit()
--- brickBreaker.py
+++ brickBreaker.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 import random
 import math
 from pygame.locals import QUIT,KEYDOWN,K_LEFT,K_RIGHT,Rect
@@ -12,7 +11,6 @@ class Block:
         self.rect = rect
         self.speed = speed
         self.dir = random.randint(-45, 45) +270
-
     def move(self):
         self.rect.centerx += math.cos(math.radians(self.dir)) * self.speed
         self.rect.centery -= math.sin(math.radians(self.dir)) * self.speed
@@ -28,24 +26,24 @@ pygame.key.set_repeat(10,10)
 screen_width = 480
 screen_height = 640
 SURFACE = pygame.display.set_mode((screen_width,screen_height))
-FPSCLOCK = pygame.time.Clock()
+clock = pygame.time.Clock()
+score_sound = pygame.mixer.Sound("D:/Project_LPG/bgm/blop_sound.mp3")
+sound = pygame.mixer.Sound("D:/Project_LPG/bgm/bgm2.mp3")
+sound.play(-1)
 
 def main():
-    Score = 0
-    BLOCK = []
-    PADDLE = Block((200,200,0),Rect((screen_width/2-50),600,100,30))
-    BALL = Block((200,200,0),Rect((screen_width/2-20),600,20,20),10)
+    score = 0
+    block = []
+    paddle = Block((200, 200, 0), Rect((screen_width / 2 - 50), 600, 100, 30))
+    ball = Block((200, 200, 0), Rect((screen_width / 2 - 20), 600, 20, 20), 10)
     colors = [(255, 0, 0), (255, 150, 0), (255, 228, 0), (11, 201, 4),(0,84,255),(0,0,147),(201,0,167)]
     for y,color in enumerate(colors,start=0):
         for x in range(0,6):
-            BLOCK.append(Block(color,Rect(x*80+12, y*40 + 10, 55, 20)))
-
-    Bigfont = pygame.font.SysFont(None, 80)
-    Smallfont = pygame.font.SysFont(None, 50)
-
-    running = True  # 게임이 진행중인가?
+            block.append(Block(color, Rect(x * 80 + 12, y * 40 + 10, 55, 20)))
+    game_font = pygame.font.SysFont(None, 50)
+    running = True
     while running:
-        M_SCORE = Smallfont.render(f"SCORE : {Score}", True, (255, 255, 255))
+        scoreT = game_font.render(f"SCORE : {score}", True, (255, 255, 255))
         SURFACE.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -53,49 +51,43 @@ def main():
                 sys.exit()
             elif event.type == KEYDOWN:
                 if event.key == K_LEFT:
-                    PADDLE.rect.centerx -= 10
+                    paddle.rect.centerx -= 10
                 elif event.key == K_RIGHT:
-                    PADDLE.rect.centerx += 10
+                    paddle.rect.centerx += 10
         else:
-            SURFACE.blit(M_SCORE, (10, 500))
-            LenBlock = len(BLOCK)
-            BLOCK = [x for x in BLOCK if not x.rect.colliderect(BALL.rect)]
-            if len(BLOCK) != LenBlock:
-                Score += 10
-                BALL.dir *= -1
-            if BALL.rect.centery < 1000:
-                BALL.move()
-
-            if PADDLE.rect.colliderect(BALL.rect):
-                BALL.speed += 0.25
-                BALL.dir = 90 + (PADDLE.rect.centerx - BALL.rect.centerx) / PADDLE.rect.width * 100
-
-            if PADDLE.rect.centerx < 55 :
-                PADDLE.rect.centerx = 55
-            if PADDLE.rect.centerx > screen_width-55:
-                PADDLE.rect.centerx = screen_width-55
-
-            if BALL.rect.centerx < 0 or BALL.rect.centerx > 480:
-                BALL.dir = 180 - BALL.dir  # 반사각만큼 방향 변화
-
-            elif BALL.rect.centery < 0:
-                BALL.dir = -BALL.dir
-
-            if len(BLOCK) == 0:
-                messagebox.showinfo("성공하셨습니다!", f"점수 : {Score}")
+            SURFACE.blit(scoreT, (10, 500))
+            LenBlock = len(block)
+            block = [x for x in block if not x.rect.colliderect(ball.rect)]
+            if len(block) != LenBlock:
+                score_sound.play()
+                score += 10
+                ball.dir *= -1
+            if ball.rect.centery < 1000:
+                ball.move()
+            if paddle.rect.colliderect(ball.rect):
+                ball.speed += 0.25
+                ball.dir = 90 + (paddle.rect.centerx - ball.rect.centerx) / paddle.rect.width * 100
+            if paddle.rect.centerx < 55 :
+                paddle.rect.centerx = 55
+            if paddle.rect.centerx > screen_width-55:
+                paddle.rect.centerx = screen_width - 55
+            if ball.rect.centerx < 0 or ball.rect.centerx > 480:
+                ball.dir = 180 - ball.dir
+            elif ball.rect.centery < 0:
+                ball.dir = -ball.dir
+            if len(block) == 0:
+                messagebox.showinfo("성공하셨습니다!", f"점수 : {score}")
                 running = False
-            if BALL.rect.centery >770 and len(BLOCK) > 0:
-                messagebox.showinfo("종료", f"점수 : {Score} \n 종료되었습니다.")
+            if ball.rect.centery >770 and len(block) > 0:
+                messagebox.showinfo("종료", f"점수 : {score} \n종료되었습니다.")
                 running = False
-
-            BALL.draw_E()
-            PADDLE.draw_R()
-            for i in BLOCK:
+            ball.draw_E()
+            paddle.draw_R()
+            for i in block:
                 i.draw_R()
 
         pygame.display.update()
-        FPSCLOCK.tick(30)
+        clock.tick(30)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
