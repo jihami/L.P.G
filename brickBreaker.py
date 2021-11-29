8b217bee7f7185be381cca0df48585c3c3428530
jihami <w2027@e-mirim.hs.kr>
2021-11-30 01:17:41 +0900

    일단 완성!

--- brickBreaker.py
+++ brickBreaker.py
@@ -3,6 +3,8 @@ import sys
 import random
 import math
 from pygame.locals import QUIT,KEYDOWN,K_LEFT,K_RIGHT,Rect
+from tkinter import *
+from tkinter import messagebox
 
 class Block:
     def __init__(self,col,rect,speed = 0):
@@ -11,21 +13,21 @@ class Block:
         self.speed = speed
         self.dir = random.randint(-45, 45) +270
 
-    def move(self):  # 공을 움직인다 , radians를 사용해서 dir을 라디안으로 변환, X축과Y축의 방향성분구분
+    def move(self):
         self.rect.centerx += math.cos(math.radians(self.dir)) * self.speed
         self.rect.centery -= math.sin(math.radians(self.dir)) * self.speed
     def draw_E(self):
         pygame.draw.ellipse(SURFACE, self.col, self.rect)
     def draw_R(self):
         pygame.draw.rect(SURFACE,self.col,self.rect)
-
+root= Tk()
+root.withdraw()
 pygame.init()
-pygame.display.set_caption('Block_Breaker!!')
+pygame.display.set_caption('벽돌깨기')
 pygame.key.set_repeat(10,10)
-screen_width = 480 #가로 크기
-screen_height = 640 #세로 크기
+screen_width = 480
+screen_height = 640
 SURFACE = pygame.display.set_mode((screen_width,screen_height))
-# SURFACE = pygame.display.set_mode((1000,800))
 FPSCLOCK = pygame.time.Clock()
 
 def main():
@@ -36,15 +38,13 @@ def main():
     colors = [(255, 0, 0), (255, 150, 0), (255, 228, 0), (11, 201, 4),(0,84,255),(0,0,147),(201,0,167)]
     for y,color in enumerate(colors,start=0):
         for x in range(0,6):
-            BLOCK.append(Block(color,Rect(x*80+10, y*40 + 30, 55, 20)))
+            BLOCK.append(Block(color,Rect(x*80+12, y*40 + 10, 55, 20)))
 
     Bigfont = pygame.font.SysFont(None, 80)
     Smallfont = pygame.font.SysFont(None, 50)
-    M_CLEAR = Bigfont.render("CLEAR!!", True, (255, 255, 255))
-    M_FAIL = Bigfont.render("FAILED", True, (255, 255, 255))
 
-
-    while True:
+    running = True  # 게임이 진행중인가?
+    while running:
         M_SCORE = Smallfont.render(f"SCORE : {Score}", True, (255, 255, 255))
         SURFACE.fill((0, 0, 0))
         for event in pygame.event.get():
@@ -65,27 +65,28 @@ def main():
                 BALL.dir *= -1
             if BALL.rect.centery < 1000:
                 BALL.move()
-            #패들과 공이 부딪힘
-            if PADDLE.rect.colliderect(BALL.rect):  # colliderect은 볼과 패들의 충돌 여부를 검사합니다.
+
+            if PADDLE.rect.colliderect(BALL.rect):
                 BALL.speed += 0.25
                 BALL.dir = 90 + (PADDLE.rect.centerx - BALL.rect.centerx) / PADDLE.rect.width * 100
 
-            #패들이 게임틀안에 존재
             if PADDLE.rect.centerx < 55 :
                 PADDLE.rect.centerx = 55
             if PADDLE.rect.centerx > screen_width-55:
                 PADDLE.rect.centerx = screen_width-55
 
-            if BALL.rect.centerx < 0 or BALL.rect.centerx > 1000:
+            if BALL.rect.centerx < 0 or BALL.rect.centerx > 480:
                 BALL.dir = 180 - BALL.dir  # 반사각만큼 방향 변화
 
             elif BALL.rect.centery < 0:
                 BALL.dir = -BALL.dir
 
             if len(BLOCK) == 0:
-                SURFACE.blit(M_CLEAR,(140,400))
+                messagebox.showinfo("성공하셨습니다!", f"점수 : {Score}")
+                running = False
             if BALL.rect.centery >770 and len(BLOCK) > 0:
-                SURFACE.blit(M_FAIL,(140,400))
+                messagebox.showinfo("종료", f"점수 : {Score} \n 종료되었습니다.")
+                running = False
 
             BALL.draw_E()
             PADDLE.draw_R()
@@ -94,6 +95,7 @@ def main():
 
         pygame.display.update()
         FPSCLOCK.tick(30)
+
 
 if __name__ == '__main__':
     main()
